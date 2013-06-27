36cb89b207930dc88fbe770a5889f1175d14afb4
Administrator <suxu@ubuntu.ubuntu-domain>
2013-06-27 15:52:43 +0800

    whenever test env

--- config/deploy-remote-test.rb
+++ config/deploy-remote-test.rb
@@ -2,16 +2,19 @@
 # 发布应用分支 master 到服务器
 #
 # 使用用法：
-#   部署：mina deploy -f config/deploy-local.rb -v
-#   停止：mina shutdown -f config/deploy-local.rb -v
+#   部署：mina deploy -f config/deploy-remote-test.rb -v
+#   停止：mina shutdown -f config/deploy-remote-test.rb -v
 #   其他命令参见：mina tasks
 #
 # **************
 # 警告：为了减少必须要的错误, 部署前请先确保应用已经被停止
-# **************10
+# **************10 yc885588
 #
 # 运行命令后输出 “in `write': Broken pipe (Errno::EPIPE)”，忽略即可
-#
+# ec2-174-129-207-99.compute-1.amazonaws.com
+
+# 日本 ec2-54-250-148-72.ap-northeast-1.compute.amazonaws.com
+# 美国 ec2-174-129-207-99.compute-1.amazonaws.com
 
 require 'mina/bundler'
 require 'mina/rails'
@@ -25,24 +28,24 @@ set_default :term_mode, :pretty
 #   deploy_to    - Path to deploy into.
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
+# ec2-54-250-148-72.ap-northeast-1.compute.amazonaws.com
+set :domain, 'ec2-54-250-148-72.ap-northeast-1.compute.amazonaws.com'
+set :deploy_to, '/home/ubuntu/apps/brood.com'
+set :user, 'ubuntu'
 
-set :domain, '127.0.0.1' #'ec2-54-242-118-120.compute-1.amazonaws.com'
-set :deploy_to, '/home/suxu/www/broot.com'
-set :user, 'suxu'
-
-set :rails_env, 'production'
+set :rails_env, 'test'
 
 set :repository, 'ubuntu@ec2-54-250-148-72.ap-northeast-1.compute.amazonaws.com:repo/brood.git'
 set :branch, 'master'
-set :rvm_path, '/home/suxu/.rvm/scripts/rvm' #'/usr/local/rvm/scripts/rvm'
+set :rvm_path, '/home/ubuntu/.rvm/scripts/rvm' #'/usr/local/rvm/scripts/rvm'
 
 # thin settings
 set_default :thin_cmd, 'thin'
-set_default :thin_config, 'config/thin.yml'
+set_default :thin_config, 'config/thin.test.yml'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_config_files, ['config/database.yml',settings.thin_config]
+set :shared_config_files, ['config/database.yml','config/thin.test.yml',settings.thin_config]
 
 set :shared_paths, ['log', 'public/uploads'] + settings.shared_config_files
 
@@ -54,7 +57,7 @@ task :environment do
   # invoke :'rbenv:load'
 
   # For those using RVM, use this to load an RVM version@gemset.
-  invoke :'rvm:use[1.9.3]'
+  invoke :'rvm:use[ruby-1.9.3-p429]'
 end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
@@ -62,9 +65,6 @@ end
 # all releases.
 task :setup => :environment do
   #queue! %[chmod g+rx,u+rwx "#{deploy_to}"]
-
-  # queue! %[mkdir -p "#{deploy_to}/current"]
-  # queue! %[chmod g+rx,u+rwx "#{deploy_to}/current"]
 
   queue! %[mkdir -p "#{deploy_to}/shared/log"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/log"]
@@ -85,11 +85,12 @@ task :deploy => :environment do
   deploy do
     # Put things that will set up an empty directory into a fully set-up
     # instance of your project.
-    invoke :'git:clone'
-    invoke :'deploy:link_shared_paths'
-    invoke :'bundle:install'
-    invoke :'rails:db_migrate'
-    invoke :'rails:assets_precompile'
+    #invoke :'git:clone'
+    #invoke :'deploy:link_shared_paths'
+    #invoke :'bundle:install'
+    #invoke :'rails:db_migrate'
+    #invoke :'db:migrate'
+    #invoke :'rails:assets_precompile'
 
 
     to :launch do
@@ -115,6 +116,7 @@ task :shutdown do
   queue %[echo "-----> Shutting down..."]
   invoke :'rvm:use[ruby-1.9.3]'
   invoke :'thin:stop'
+  invoke :'whenever:clear'
 end
 
 #
