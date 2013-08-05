bc2264dda33aad33ddbf56aca3dce377ae3e3e45
Administrator <suxu@ubuntu.ubuntu-domain>
2013-08-05 18:19:19 +0800

    t

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 module ApplicationHelper
 	
 	def time_str time
@@ -6,6 +7,19 @@ module ApplicationHelper
 	
 	def ip_to_url ip
 		ip.gsub(".","_") if ip
+	end
+
+	def event_badge event,times
+		cls = ""
+		cls = "badge-info" if ["online","offline","reopen","pay","reset_ip","reset_role"].include?(event)
+		cls = "badge-success" if event == "success"
+		cls = "badge-warning" if ["lock","AnswerVerifyCode"].include?(event) 
+		cls = "badge-important" if event == "close"
+		cls = "badge-inverse" if event  == "lose"
+		html = "<span class='badge #{cls}'>#{event}</span>"
+		html = html + "(#{times})次" if times > 0
+		return html.html_safe
+
 	end
 
 end
