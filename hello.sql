8b71c84db0d744b8b3060215794c4b1fed781b05
Administrator <suxu@ubuntu.ubuntu-domain>
2013-08-16 18:13:48 +0800

    delete hello.sql

--- hello.sql
+++
@@ -1,2 +0,0 @@
-id	account	password	role_index	server	level	vit_power	status	computer_id	count	online	ip	gold	created_at	updated_at	close	close_hours	closed_at	reopen_at	total	total_pay
-1	1454229417	xyz987987	0	四川1区	45	156	1	0	0	0	NULL	0	2013-07-15 10:02:33	2013-07-15 10:02:33	0	NULL	NULL	NULL	0	0
