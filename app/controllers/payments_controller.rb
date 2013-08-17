4f44b6f4b5adaa55855caaa6ccd61d1d69422376
Administrator <suxu@ubuntu.ubuntu-domain>
2013-08-17 10:50:45 +0800

    gold show link_to payments search by server & pay_type

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -20,6 +20,7 @@ class PaymentsController < ApplicationController
 			@payments = @payments.where("created_at <= '#{params[:max_time]}'") unless params[:max_time].blank?
 			@payments = @payments.where("gold >= #{params[:min_gold]}") unless params[:min_gold].blank?
 			@payments = @payments.where("gold <= #{params[:max_gold]}") unless params[:max_gold].blank?
+			@payments = @payments.where(:server => params[:server]) unless params[:server].blank?
 			@payments = @payments.where(:pay_type => params[:pay_type]) unless params[:pay_type].blank?
 			@payments = @payments.where("remark like ?","%#{params[:remark]}%") unless params[:remark].blank?
 			
