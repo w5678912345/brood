dce28f3687f28373b29403ba823888057f80ea7d
鑫 李 <rosebush@xindeMacBook-Pro.local>
2013-09-17 17:49:34 +0800

    tmp

--- Gemfile
+++ Gemfile
@@ -2,10 +2,10 @@ source 'https://rubygems.org'
 #source 'http://ruby.taobao.org'
 gem 'rails', '3.2.13'
 
-# Bundle edge Rails instead:
+# Bundle edge Rails instead:   hello
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 #gem 'rack', '~> 1.4.5'
-gem 'mysql2'
+gem 'mysql2','~> 0.3.13'
 
 
 # Gems used only for assets and not required
--- app/controllers/analysis/multiday_controller.rb
+++ app/controllers/analysis/multiday_controller.rb
@@ -2,6 +2,17 @@
 class Analysis::MultidayController < Analysis::AppController
 
 	def show
+		@date = Time.now - 7.day
+		@end_date = Time.now
+
+		date = params[:date] if not params[:date].blank?
+		end_date = params[:end_date] if not params[:end_date].blank?
+		@sevenS = Note.select("date(created_at) as Day,sum(if(api_name = 'success',1,0)) as SuccCount,
+				sum(if(api_name = 'online',1,0)) as OnlineCount,
+				sum(if(api_name = 'close',1,0)) as CloseCount,
+				sum(if(api_name = 'AnswerVerifyCode',1,0)) as VerifyCodeCount
+			").group("date(created_at)").
+			time_scope(params[:date],params[:end_date])
 
 	end
 
