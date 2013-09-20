ae6c910f6f958fa58b825a350d4c0984f1c6e9e8
鑫 李 <rosebush@xindeMacBook-Pro.local>
2013-09-20 13:10:00 +0800

    添加对转出金币的按日统计

--- app/controllers/analysis/multiday_controller.rb
+++ app/controllers/analysis/multiday_controller.rb
@@ -2,18 +2,27 @@
 class Analysis::MultidayController < Analysis::AppController
 
 	def show
-		@date = Time.now - 7.day
-		@end_date = Time.now
+		@start_time = Time.now - 7.day
+		@end_time = Time.now + 1.day
 
-		date = params[:date] if not params[:date].blank?
-		end_date = params[:end_date] if not params[:end_date].blank?
+		@start_time = params[:date] if not params[:date].blank?
+		@end_time = params[:end_date] if not params[:end_date].blank?
+
 		@sevenS = Note.select("date(created_at) as Day,sum(if(api_name = 'success',1,0)) as SuccCount,
 				sum(if(api_name = 'online',1,0)) as OnlineCount,
 				sum(if(api_name = 'close',1,0)) as CloseCount,
 				sum(if(api_name = 'AnswerVerifyCode',1,0)) as VerifyCodeCount
 			").group("date(created_at)").
-			time_scope(params[:date],params[:end_date])
-
+			time_scope(@start_time,@end_time).order("date(created_at)")
+		
+		tradetemp = Payment.select("date(created_at) as Day,sum(gold) Gold").group("date(created_at)").
+						time_scope(@start_time,@end_time).order("date(created_at)")
+		@trade = {}
+		@tradesum = 0
+		tradetemp.each do |i|
+			@trade[i.Day] = i.Gold
+			@tradesum += i.Gold
+		end
 	end
 
 
