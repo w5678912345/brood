f01e19b077e15795cf6cc8a05356a85534f5f5f4
suxu <suxu@hi.local>
2013-11-11 16:37:34 +0800

    t

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -9,7 +9,15 @@ class PaymentsController < ApplicationController
 		
 		def index
 			@payments = Payment.includes(:role).order("id DESC")
-			@payments = @payments.where(:pay_type => params[:type]) unless params[:type].blank?
+			@payments = @payments.where(:role_id => params[:role_id]) unless params[:role_id].blank?
+			@payments = @payments.where("created_at >= '#{params[:min_time]}'") unless params[:min_time].blank?
+			@payments = @payments.where("created_at <= '#{params[:max_time]}'") unless params[:max_time].blank?
+			@payments = @payments.where("gold >= #{params[:min_gold]}") unless params[:min_gold].blank?
+			@payments = @payments.where("gold <= #{params[:max_gold]}") unless params[:max_gold].blank?
+			@payments = @payments.where(:server => params[:server]) unless params[:server].blank?
+			@payments = @payments.where(:pay_type => params[:pay_type]) unless params[:pay_type].blank?
+			@payments = @payments.where("remark like ?","%#{params[:remark]}%") unless params[:remark].blank?
+			
 			@payments = @payments.paginate(:page => params[:page],:per_page => 20)
 		end
 		
