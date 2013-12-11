d607ed908b941cb6c6978b564d1f4d6607aeb3c0
suxu <suxu@hi.local>
2013-12-11 17:40:54 +0800

    t

--- app/models/account_role.rb
+++ app/models/account_role.rb
@@ -9,12 +9,21 @@ class AccountRole
 		records = records.where("accounts.status in (?)",opts[:ass]) unless opts[:ass].blank?
 		records = records.where("accounts.roles_count = ?",opts[:roles_count].to_i) unless opts[:roles_count].blank?
 		records = records.where("accounts.bind_computer_id = ?",opts[:bind_cid].to_i) unless opts[:bind_cid].blank?
+		unless opts[:started].blank?
+			records = records.where("accounts.session_id = 0") if opts[:started].to_i == 0
+			records = records.where("accounts.session_id > 0") if opts[:started].to_i == 1
+		end
 		records = records.where("date(accounts.created_at) = ?",opts[:a_created_at]) unless opts[:a_created_at].blank?
 		#
 		records = records.where("roles.id = ?",opts[:rid].to_i) unless opts[:rid].blank?
 		records = records.where("roles.role_index = ?",opts[:index].to_i) unless opts[:index].blank?
 		records = records.where("roles.status in (?)",opts[:rss]) unless opts[:rss].blank?
 		records = records.where("date(roles.created_at) = ?",opts[:r_created_at]) unless opts[:r_created_at].blank?
+		records = records.where("roles.online = ?",opts[:online]) unless opts[:online].blank?
+		unless opts[:r_started].blank?
+			records = records.where("roles.session_id = 0") if opts[:r_started].to_i == 0
+			records = records.where("roles.session_id > 0") if opts[:r_started].to_i == 1
+		end
 		unless opts[:level].blank?
 	      tmp = opts[:level].split("-")
 	      records = tmp.length == 2 ? records.where("roles.level >= ? and roles.level <= ?",tmp[0],tmp[1]) : records.where("roles.level =? ",opts[:level].to_i)
