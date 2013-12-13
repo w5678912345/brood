fb65b03a815274f6ae46eb512ed0e6bee883301d
suxu <suxu@hi.local>
2013-12-13 11:34:41 +0800

    account_roles.search

--- app/controllers/account_roles_controller.rb
+++ app/controllers/account_roles_controller.rb
@@ -4,9 +4,6 @@ class AccountRolesController < ApplicationController
 	def index
 		@records = AccountRole.get_list(params)
 		params[:per_page] = params[:per_page].blank? ? 20 : params[:per_page].to_i
-		#params[:per_page] = @records.count unless params[:all].blank?
-		@v = "accounts/table"
-		@v = "roles/table" if params[:tag] == "role"
 		params[:per_page] = @records.count unless params[:all].blank?
 		@records = @records.paginate(:page => params[:page], :per_page => params[:per_page])
 	end
--- app/models/account_role.rb
+++ app/models/account_role.rb
@@ -6,20 +6,30 @@ class AccountRole
 		
 		records = opts[:tag] == "role" ? get_roles_list : get_accounts_list
 		records = records.where("accounts.id > 0")
+		records = records.where("accounts.no = ?",opts[:no]) unless opts[:no].blank?
 		records = records.where("accounts.status in (?)",opts[:ass]) unless opts[:ass].blank?
+		records = records.where("accounts.server like ?","%#{opts[:server]}%") unless opts[:server].blank?
 		records = records.where("accounts.roles_count = ?",opts[:roles_count].to_i) unless opts[:roles_count].blank?
 		records = records.where("accounts.bind_computer_id = ?",opts[:bind_cid].to_i) unless opts[:bind_cid].blank?
+		records = records.where("accounts.today_success =?",opts[:ts].to_i) unless opts[:ts].blank?
+		records = records.where("accounts.online_ip = ?",opts[:online_ip]) unless opts[:online_ip].blank?
+		records = records.where("accounts.online_role_id =?",opts[:online_rid]) unless opts[:online_rid].blank?
 		unless opts[:started].blank?
 			records = records.where("accounts.session_id = 0") if opts[:started].to_i == 0
 			records = records.where("accounts.session_id > 0") if opts[:started].to_i == 1
 		end
+		unless opts[:bind].blank?
+			records = opts[:bind] == "bind" ? records.where("bind_computer_id > 0") : records.where("accounts.bind_computer_id =?",opts[:bind].to_i)
+		end
 		records = records.where("date(accounts.created_at) = ?",opts[:a_created_at]) unless opts[:a_created_at].blank?
 		#
 		records = records.where("roles.id = ?",opts[:rid].to_i) unless opts[:rid].blank?
+		records = records.where("roles.server like ?","%#{opts[:server]}%") unless opts[:r_server].blank?
 		records = records.where("roles.role_index = ?",opts[:index].to_i) unless opts[:index].blank?
 		records = records.where("roles.status in (?)",opts[:rss]) unless opts[:rss].blank?
 		records = records.where("date(roles.created_at) = ?",opts[:r_created_at]) unless opts[:r_created_at].blank?
 		records = records.where("roles.online = ?",opts[:online]) unless opts[:online].blank?
+		records = records.where("roles.today_success =?",opts[:rts].to_i) unless opts[:rts].blank?
 		unless opts[:r_started].blank?
 			records = records.where("roles.session_id = 0") if opts[:r_started].to_i == 0
 			records = records.where("roles.session_id > 0") if opts[:r_started].to_i == 1
