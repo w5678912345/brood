9861ab45e84a3e921625332926088609dce0f1ac
suxu <suxu.info@gmail.com>
2013-12-20 00:27:45 +0800

    ability.r

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,7 +8,7 @@ class Ability
 		else
 			#can :read, :all
             can [:index],   Account
-            can [:index],    Note
+            can [:index],   Note
 			can [:index],   User
 			can [:index,:show,:home,:online,:offline,:closed,:not_closed,:notes,:payments,:search,:warting], Role
 			can [:index,:show,:home,:checked,:unchecked,:notes,:roles],	Computer
