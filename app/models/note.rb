477bfd2e06cda59c76499daca9c615092e627694
suxu <suxu@hi.local>
2014-01-22 11:55:13 +0800

    note search by level

--- app/models/note.rb
+++ app/models/note.rb
@@ -89,6 +89,10 @@ class Note < ActiveRecord::Base
         notes = notes.where(:ending => opts[:end].to_i) unless opts[:end].blank?
         notes = notes.where(:success => opts[:success].to_i) unless opts[:success].blank?
         notes = notes.where(:result => opts[:result]) unless opts[:result].blank?
+        unless opts[:level].blank?
+          tmp = opts[:level].split("-")
+          notes = tmp.length == 2 ? notes.where("level >= ? and level <= ?",tmp[0],tmp[1]) : notes.where("level =? ",opts[:level].to_i)
+        end
         return notes
     end
 
