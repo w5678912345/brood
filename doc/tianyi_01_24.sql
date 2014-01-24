78dbee8f98f7371b6777f7ed19551d5c9a03a5c9
suxu <suxu@hi.local>
2014-01-24 17:06:01 +0800

    hl

--- doc/tianyi_01_24.sql
+++ doc/tianyi_01_24.sql
@@ -23,3 +23,25 @@ update computers set accounts_count =0 ,online_accounts_count = 0;
 
 update accounts inner join roles on accounts.no = roles.account set accounts.server = roles.server
 	where accounts.server = '2014-01-24 07:00:00' and roles.role_index = 0;
+
+
+select date(accounts.created_at) as d,count(accounts.id) as cc from accounts left join notes on accounts.no = notes.account
+where notes.api_name= 'discardforyears' and date(notes.created_at) = '2014-01-23' group by date(accounts.created_at) order by d;
+
+
+
+select date(accounts.created_at) as _date,count(accounts.id) as _count from accounts left join notes on accounts.no = notes.account
+ where notes.api_name = 'account_start' and notes.created_at >= '2014-01-23 16:00:00' and notes.created_at <= '2014-01-23 20:00:00'
+ group by date(accounts.created_at) order by _count;
+
++---------------------------+--------------------+
+| date(accounts.created_at) | count(accounts.id) |
++---------------------------+--------------------+
+| 2013-11-27                |                 58 |
+| 2013-12-09                |                  5 |
+| 2013-12-13                |                191 |
+| 2013-12-15                |                 49 |
+| 2013-12-24                |                  4 |
+| 2013-12-31                |                 28 |
+| 2014-01-12                |                  1 |
++---------------------------+--------------------+
