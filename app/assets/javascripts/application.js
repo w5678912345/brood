5b2bfe01240795883fbc64cbe8592e9037d85f86
suxu <suxu@hi.local>
2014-02-07 14:12:15 +0800

    datetimepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,8 +2,12 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.core
+//= require jquery.ui.widget
+//= require jquery.ui.mouse
+//= require jquery.ui.slider
 //= require jquery.ui.datepicker
 //= require jquery.ui.datepicker-zh-CN
+//= require jquery.ui.timepicker.addon
 //= require bootstrap
 //= require jquery.validate
 //= require jquery.validate.messages_zh
