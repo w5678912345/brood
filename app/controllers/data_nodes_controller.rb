706ead1f698f6091a56345660d795880ab842d65
鑫 李 <rosebush@xindeMacBook-Pro.local>
2014-02-23 23:31:22 +0800

    修改报表页面和增加新computer事件QQRegister

--- app/controllers/data_nodes_controller.rb
+++ app/controllers/data_nodes_controller.rb
@@ -22,19 +22,35 @@ class DataNodesController < ApplicationController
 
 
 		@records = DataNode.select("date(created_at) as day,accounts").date_scope(@start_date,@end_date).order("created_at asc").group("date(created_at)")
-		@status= Account::STATUS.keys
+		@status= ["normal","bslocked","locked","discardforyears"]#Account::STATUS.keys
 		
 		@d = []
 
 		@days = @records.map(&:day)
+		normal_data = {}
 		@status.each do |s|
 			_data = []
+			_delta = 0
 			@records.each do |r|
 				tmp = eval(r.accounts)
-				_data<< (tmp[s.to_s] || 0)
+				if s == 'normal'
+					_data << (tmp[s.to_s] || 0)
+				else
+					_delta = tmp[s.to_s] - _delta
+					_data<< _delta#((tmp[s.to_s] || 0))
+					_delta = tmp[s.to_s] 
+				end
 			end
-			@d << {:name=>s,:data =>_data}
+			type = 'column'
+			yAxis = 0
+			if s == 'normal' 
+				type = 'line' 
+				normal_data = {:type => type,:yAxis => yAxis,:name=>s,:stack=>'good',:data =>_data}
+			else
+				@d << {:type => type,:yAxis => yAxis,:name=>s,:stack=>'bad',:data =>_data}
+			end
 		end
+		@d << normal_data
 
 		#render :text => @d.as_json
 		# :text => @d
