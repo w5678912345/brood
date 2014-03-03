a4757ec0665336500f1e93afcee35f595fb3c018
suxu <suxu@hi.local>
2014-03-03 16:03:48 +0800

    years

--- app/models/account.rb
+++ app/models/account.rb
@@ -140,7 +140,7 @@ class Account < ActiveRecord::Base
         end
         # 记录账号发生的事件
         api_name = event if EVENT.include? event # 如果定义了有效事件，设置api_name => event
-        if status == 'discardfordays'
+        if status == 'discardfordays' || status == "discardforyears"
             count = Note.where(:api_name=>"discardfordays").where("date(created_at)=?",Date.today.to_s).count
             computer.update_attributes(:status=>0) if count >= Setting.account_discardfordays
         end
