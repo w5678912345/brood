0a804ee21d6d0056391c236743772e6bd70e4c1b
suxu <suxu@hi.local>
2014-03-03 13:30:43 +0800

    search by time

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,6 +2,9 @@
 class NotesController < ApplicationController
 	
 	def index
+		now = Time.now
+		params[:start_time] = now.change(:hour => 0,:min => 0,:sec => 0).strftime("%Y-%m-%d %H:%M:%S") if params[:start_time].blank?
+		params[:end_time] = now.since(1.day).change(:hour => 0,:min => 0,:sec => 0).strftime("%Y-%m-%d %H:%M:%S") if params[:end_time].blank?
 		@notes = Note.list_search(params)
 		per_page = params[:per_page].blank? ? 20 : params[:per_page].to_i
 		@notes = @notes.paginate(:page => params[:page], :per_page => per_page)
