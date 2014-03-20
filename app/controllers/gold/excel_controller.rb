bbed776cff3bccab4ef51b59243067a4bfa245ac
suxu <suxu@hi.local>
2014-03-20 17:50:06 +0800

    gold/excel

--- app/controllers/gold/excel_controller.rb
+++ app/controllers/gold/excel_controller.rb
@@ -11,6 +11,18 @@ class ExcelController < AppController
 		@records = @records.where("server like ?","#{params[:server]}%") unless params[:server].blank?
 		@records = @records.group("date(created_at)").time_scope(params[:start_time],params[:end_time]).order("date(created_at)")
 
+		@trade = {}
+		@tradesum = 0
+		@records.each do |i|
+			@trade[i.Day.to_s] = i.Gold
+			@tradesum += i.Gold
+		end
+
+		start_time  = Date.parse(params[:start_time])
+		end_time = Date.parse(params[:end_time])
+
+		@dates = (start_time..end_time).to_a
+		
 	end
 
 end
