a8ff292aa40905f1329ef21999c05cf1bc2beb91
鑫 李 <rosebush@xindeMacBook-Pro.local>
2014-05-16 15:51:00 +0800

    today异常事件bug

--- app/controllers/todays_controller.rb
+++ app/controllers/todays_controller.rb
@@ -4,7 +4,7 @@ class TodaysController < ApplicationController
 		@online_role_count = RoleSession.count
 		@today_trade_gold = Payment.trade_scope.at_date(Date.today).sum(:gold)
 		#.at_date(Date.today)
-		@error_event_count = Note.select("api_name,count(*)").group("api_name").at_date(Date.today).event_scope(["discardforyears","exception"]).count
+		@error_event_count = Note.select("api_name,count(*) as num").group("api_name").at_date(Date.today).event_scope(["discardforyears","exception"])
 		#binding.pry
 		@finished_role_count = HistoryRoleSession.at_date(Date.today).count(:role_id,:distinct => true)
 		@can_use_role_count = Role.joins(:qq_account).where("accounts.status = 'normal'").count
