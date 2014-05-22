3c7fba63e4839f7244ec7b94687b5eaa6de2d19e
suxu <suxu@hi.local>
2014-05-22 11:27:33 +0800

    deploy-bolt

--- config/deploy-test-bolt.rb
+++ config/deploy-test-bolt.rb
@@ -57,7 +57,7 @@ task :environment do
   # invoke :'rbenv:load'
 
   # For those using RVM, use this to load an RVM version@gemset.
-  invoke :'rvm:use[ruby-1.9.3-p429]'
+  invoke :'rvm:use[ruby-2.0.0-p247@brood]'
 end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
@@ -115,7 +115,7 @@ end
 desc "Shutdown app."
 task :shutdown do
   queue %[echo "-----> Shutting down..."]
-  invoke :'rvm:use[ruby-1.9.3]'
+  invoke :'rvm:use[ruby-2.0.0-p247@brood]'
   invoke :'thin:stop'
   queue %[whenever -c]
 end
