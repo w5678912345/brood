847af60e90ba38b52485882610374f4e3196e975
suxu <suxu@hi.local>
2014-05-27 10:36:29 +0800

    la

---
+++ doc/05_27.sql
@@ -0,0 +1,8 @@
+
+
+
+select count(n2.id) as cc, n2.account from notes as n1
+  inner join notes as n2 on n1.account = n2.account 
+  where n1.api_name = 'discardforyears' and date(n1.created_at) = '2014-05-26' 
+  and  n2.api_name = 'account_start' and n2.success = 1 and date(n2.created_at) >= '2014-05-22' 
+  group by n2.account order by cc
