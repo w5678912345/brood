45d7ca3aaaf0e9e691742f80ad99bd006ddc863d
鑫 李 <rosebush@xindeMacBook-Pro-3.local>
2015-01-22 14:50:17 +0800

    修改发布，特别是tian6

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,7 @@
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
+require 'mina/rvm'
 # require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
 # require 'mina/rvm'    # for rvm support. (http://rvm.io)
 
@@ -9,62 +10,29 @@ require 'mina/git'
 #   deploy_to    - Path to deploy into.
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
+set :tian,'test_tianyi.dabi.co'
+set :bolt,'www.dabi.co'
+set :domain, bolt
+set :deploy_path, 'apps/test_bolt.dabi.co/current'
 
-set :domain, 'foobar.com'
-set :deploy_to, '/var/www/foobar.com'
-set :repository, 'git://...'
-set :branch, 'master'
+set :target ,ENV["TARGET"]
 
-# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
-# They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'log']
+set :rvm_path, '/home/ubuntu/.rvm/bin/rvm' #'/usr/local/rvm/scripts/rvm'
 
 # Optional settings:
 #   set :user, 'foobar'    # Username in the server to SSH to.
 #   set :port, '30000'     # SSH port number.
-
-# This task is the environment that is loaded for most commands, such as
-# `mina deploy` or `mina rake`.
-task :environment do
-  # If you're using rbenv, use this to load the rbenv environment.
-  # Be sure to commit your .rbenv-version to your repository.
-  # invoke :'rbenv:load'
-
-  # For those using RVM, use this to load an RVM version@gemset.
-  # invoke :'rvm:use[ruby-1.9.3-p125@default]'
-end
-
-# Put any custom mkdir's in here for when `mina setup` is ran.
-# For Rails apps, we'll make some of the shared paths that are shared between
-# all releases.
-task :setup => :environment do
-  queue! %[mkdir -p "#{deploy_to}/shared/log"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/log"]
-
-  queue! %[mkdir -p "#{deploy_to}/shared/config"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/config"]
-
-  queue! %[touch "#{deploy_to}/shared/config/database.yml"]
-  queue  %[-----> Be sure to edit 'shared/config/database.yml'.]
-end
+set :user, 'ubuntu'
 
 desc "Deploys the current version to the server."
-task :deploy => :environment do
-  deploy do
-    # Put things that will set up an empty directory into a fully set-up
-    # instance of your project.
-    invoke :'git:clone'
-    invoke :'deploy:link_shared_paths'
-    invoke :'bundle:install'
-    invoke :'rails:db_migrate'
-    invoke :'rails:assets_precompile'
-
-    to :launch do
-      queue 'touch tmp/restart.txt'
-    end
-  end
+task :r_deploy do
+  invoke :'rvm:use[ruby-2.0.0-p247]'
+  queue! %[cd #{deploy_path}]
+  queue! %[pwd]
+  #queue %[#{target}]
+  queue! %[mina shutdown -f config/deploy-#{target}.rb]
+  queue! %[mina deploy -f config/deploy-#{target}.rb]
 end
-
 # For help in making your deploy script, see the Mina documentation:
 #
 #  - http://nadarei.co/mina
