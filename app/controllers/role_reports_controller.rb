bff2b9818a0ee9b51107c3e27f638b212dd330ac
鑫 李 <rosebush@xindeMacBook-Pro.local>
2015-10-19 16:09:43 +0800

    修改统计去掉joinaccount，提高速度

--- app/controllers/role_reports_controller.rb
+++ app/controllers/role_reports_controller.rb
@@ -2,11 +2,11 @@ class RoleReportsController < ApplicationController
   def index
   end
   def show
-    @cols = {"accounts.server"=>"服务器","roles.profession" => "职业","roles.level"=>"等级"} 
+    @cols = {"roles.profession" => "职业","roles.level"=>"等级"} 
     @col = params[:col] || "accounts.server"
     @status = ["discardfordays","discardforyear","discardforweek","disconnect"]
-
-    @records = HistoryRoleSession.joins([:role ,:account]).select("count(*) as roles_count, #{@col} as col").group(@col)
+    params[:status] = 'disconnect' if not params[:status].present?
+    @records = HistoryRoleSession.joins(:role).select("count(*) as roles_count, #{@col} as col").group(@col)
     .reorder("roles_count desc").where("history_role_sessions.created_at > ?",Time.now.beginning_of_day)
     @records = @records.where(:server => params[:server]) if params[:server].present?
     @records = @records.where("accounts.status" => params[:status]) if params[:status].present?
