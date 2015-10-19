a589409f5bba996ec2b031af8c005b879346d0a9
鑫 李 <rosebush@xindeMacBook-Pro.local>
2015-10-19 16:53:48 +0800

    fix bug

--- app/controllers/role_reports_controller.rb
+++ app/controllers/role_reports_controller.rb
@@ -3,12 +3,12 @@ class RoleReportsController < ApplicationController
   end
   def show
     @cols = {"roles.profession" => "职业","roles.level"=>"等级"} 
-    @col = params[:col] || "accounts.server"
+    @col = params[:col] || "roles.profession"
     @status = ["discardfordays","discardforyear","discardforweek","disconnect"]
     params[:status] = 'disconnect' if not params[:status].present?
     @records = HistoryRoleSession.joins(:role).select("count(*) as roles_count, #{@col} as col").group(@col)
     .reorder("roles_count desc").where("history_role_sessions.created_at > ?",Time.now.beginning_of_day)
     @records = @records.where(:server => params[:server]) if params[:server].present?
-    @records = @records.where("accounts.status" => params[:status]) if params[:status].present?
+    @records = @records.where("result" => params[:status]) if params[:status].present?
   end
 end
