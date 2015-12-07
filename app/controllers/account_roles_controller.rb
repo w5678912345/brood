0ad62c3cb88aa131ecb4cde1cc1e33257c3002c4
鑫 李 <rosebush@xindeMacBook-Pro.local>
2015-12-07 17:03:24 +0800

    fix bugs 2

--- app/controllers/account_roles_controller.rb
+++ app/controllers/account_roles_controller.rb
@@ -6,7 +6,7 @@ class AccountRolesController < ApplicationController
 		params[:per_page] = params[:per_page].blank? ? 20 : params[:per_page].to_i
 		params[:per_page] = @records.count unless params[:all].blank?
 		@records = initialize_grid(@records,
-			:include => [:computer],
+			:include => [:bind_computer],
 			:per_page=>params[:per_page])#@records.paginate(:page => params[:page], :per_page => params[:per_page])
 	end
 
