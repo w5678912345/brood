765b1f4c1922472463adf7c4fb00cad8603f184c
鑫 李 <rosebush@xindeMacBook-Pro.local>
2015-12-14 16:19:19 +0800

    fix bug

--- app/controllers/account_roles_controller.rb
+++ app/controllers/account_roles_controller.rb
@@ -5,9 +5,15 @@ class AccountRolesController < ApplicationController
 		@records = AccountRole.get_list(params)
 		params[:per_page] = params[:per_page].blank? ? 20 : params[:per_page].to_i
 		params[:per_page] = @records.count unless params[:all].blank?
-		@records = initialize_grid(@records,
-			:include => [:bind_computer],
-			:per_page=>params[:per_page])#@records.paginate(:page => params[:page], :per_page => params[:per_page])
+		if params[:tag] == "role"
+			@records = initialize_grid(@records,
+				:include => [:qq_account => :bind_computer],
+				:per_page=>params[:per_page])#@records.paginate(:page => params[:page], :per_page => params[:per_page])
+		else
+			@records = initialize_grid(@records,
+				:include => [:bind_computer],
+				:per_page=>params[:per_page])#@records.paginate(:page => params[:page], :per_page => params[:per_page])
+		end
 	end
 
 
