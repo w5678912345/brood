aea7e22f70838a30abbacea1efcc814c6fa8aca3
鑫 李 <rosebush@xindeMacBook-Pro.local>
2016-05-13 17:39:05 +0800

    support iccid is all smsa

--- app/controllers/api/phone_tasks_controller.rb
+++ app/controllers/api/phone_tasks_controller.rb
@@ -18,14 +18,14 @@ class Api::PhoneTasksController < Api::BaseController
     end
   end
   def get_by_iccid
-    phone = Phone.find_by_iccid params[:iccid]
-    @phone_tasks = []
-    if phone
-      phone.update_attributes :last_active_at => Time.now
-      phone_tasks_query = phone.phone_tasks.where(:status => 'waiting')
+      phone_ids = Phone.where(:iccid => params[:iccid]).map &:id
+      @phone_tasks=[]
+      return if phone_ids.empty?
+      Phone.where(:no => phone_ids).update_all(:last_active_at => Time.now)
+
+      phone_tasks_query = PhoneTask.where(:phone_id => phone_ids,:status => 'waiting')
       @phone_tasks = phone_tasks_query.all 
       phone_tasks_query.update_all(:status => 'sending')
-    end
   end
   def finish
     @phone_task = PhoneTask.find_by_id params[:phone_task_id]
